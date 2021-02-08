bd80d597a2e9b48beaf6d39758b6fe72a390b925
jashimgit <jashim.ncn@gmail.com>
2021-02-08 13:02:32 +0600

    get lyric button working now

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const search = () => {
 
 const getSongs = (songs) => {
     const songscontainer = document.getElementById('songs-container')
-
+    songscontainer.innerHTML = '';
     songs.forEach(song => {
-
+        
         const songItem = document.createElement('div');
         songItem.className = "single-result row align-items-center my-3 p-3";
         songItem.innerHTML =
@@ -27,13 +27,28 @@ const getSongs = (songs) => {
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
             </div>
             <audio controls>
-            <source src="${song.preview}" type="audio/mp3">
+            <source src="${song.preview}" type="audio/mpeg">
             </audio>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyrics('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>`;
-            songscontainer.appendChild(songItem);
+        songscontainer.appendChild(songItem);
     });
 
     // songscontainer.innerHTML = songItem
+}
+
+const getLyrics = (artist, title) => {
+    // lyric link: https://api.lyrics.ovh/v1/:artist/:title
+
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLyric(data.lyrics))
+
+
+}
+const displayLyric = lyrics => {
+    const lyric = document.getElementById('song-lyric');
+    lyric.innerText = lyrics;
 }
